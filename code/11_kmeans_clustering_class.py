1f3b3e2547edae3c9e6d439ec13ee737bbb30476
Josiah Davis <josiah.j.davis@gmail.com>
2014-11-14 07:44:29 -0500

    Added exercise solutions

--- code/11_kmeans_clustering_class.py
+++ code/11_kmeans_clustering_class.py
@@ -12,9 +12,19 @@ from sklearn import datasets
 import pandas as pd
 import numpy as np
 
+# ------------------------------------------
+# EXERCISE: Compute the centoid of the following data
+#           [2, 5], [4, 4], [3, 3]
+# ------------------------------------------
+
+d = np.array([[2, 5], [4, 4], [3, 3]])
+x, y = d.mean(axis=0)
+
 # Import iris data
 iris = datasets.load_iris()
 d = iris.data
+
+np.random.seed(0)
 
 # Run KMeans
 est = KMeans(n_clusters=3, init='random')
@@ -22,12 +32,19 @@ est.fit(d)
 y_kmeans = est.predict(d)
 
 colors = np.array(['#FF0054','#FBD039','#23C2BC'])
+plt.figure()
 plt.scatter(d[:, 2], d[:, 0], c=colors[y_kmeans], s=50)
 plt.xlabel(iris.feature_names[2])
 plt.ylabel(iris.feature_names[0])
 
-# EXERCISE: Find the centers and plot them on the same graph.
+# ------------------------------------------
+# EXERCISE: Find the centers and plot them 
+#           on the same graph.
+# ------------------------------------------
 
+centers = est.cluster_centers_
+plt.scatter(centers[:, 2], centers[:, 0], c='k', linewidths=3,
+            marker='+', s=300)
 
 '''
 VISUALIZING THE CLUSTERS
@@ -41,7 +58,7 @@ multi-dimensional data? Let's look at three ways you can do this.
 #================================
 # Option #1: Scatter Plot Grid
 plt.figure(figsize=(8, 8))
-
+plt.suptitle('Scatter Plot Grid',  fontsize=14)
 # Upper Left
 plt.subplot(221)
 plt.scatter(d[:,2], d[:,0], c = colors[y_kmeans])
@@ -65,6 +82,7 @@ plt.xlabel(iris.feature_names[3])
 #================================
 # Option #2: 3d plot
 from mpl_toolkits.mplot3d import Axes3D
+plt.suptitle('3d plot', fontsize=15)
 ax = Axes3D(plt.figure(figsize=(10, 9)), rect=[.01, 0, 0.95, 1], elev=30, azim=134)
 ax.scatter(d[:,0], d[:,1], d[:,2], c = colors[y_kmeans], s=120)
 ax.set_xlabel('Sepal Width')
@@ -73,11 +91,23 @@ ax.set_zlabel('Petal Length')
 # Modified from the example here: 
 # http://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_iris.html
 
-# EXERCISE: Create a Parallel Coordinates visualization
+# ---------------------------------------
+# EXERCISE: Create a Parallel Coordinates 
+#           visualization with the classes
+# ---------------------------------------
+
 
 #================================
 # Option 3: Parallel Coordinates
 
+from pandas.tools.plotting import parallel_coordinates
+# I'm going to convert to a pandas dataframe
+# Using a snippet of code we learned from one of Kevin's lectures!
+features = [name[:-5].title().replace(' ', '') for name in iris.feature_names]
+iris_df = pd.DataFrame(iris.data, columns = features)
+iris_df['Name'] = iris.target_names[iris.target]
+parallel_coordinates(data=iris_df, class_column='Name', 
+                     colors=('#FF0054', '#FBD039', '#23C2BC'))
                      
 '''
 DETERMINING THE NUMBER OF CLUSTERS
@@ -105,14 +135,30 @@ plt.figure(figsize=(7, 8))
 plt.subplot(211)
 plt.title('Using the elbow method to inform k choice')
 plt.plot(k_rng[1:], silhouette_score, 'b*-')
-plt.xlim([1,20])
+plt.xlim([1,15])
 plt.grid(True)
 plt.ylabel('Silhouette Coefficient')
 plt.plot(3,silhouette_score[1], 'o', markersize=12, markeredgewidth=1.5,
          markerfacecolor='None', markeredgecolor='r')
 
-# EXERCISE: Calculate the sum of squared errors and plot over a range of k
+# -----------------------------------------------------
+# EXERCISE: Calculate the within sum of squared errors 
+#           and plot over a range of k
+# -----------------------------------------------------
+
 
 #================================
 # Option 2: Within Sum of Squares (a.k.a., inertia)
 # Generally want to minimize WSS, while also minimizing k
+
+within_sum_squares = [e.inertia_ for e in est]
+
+# Plot the results
+plt.subplot(212)
+plt.plot(k_rng, within_sum_squares, 'b*-')
+plt.xlim([1,15])
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('Within Sum of Squares')
+plt.plot(3,within_sum_squares[2], 'ro', markersize=12, markeredgewidth=1.5,
+         markerfacecolor='None', markeredgecolor='r')
